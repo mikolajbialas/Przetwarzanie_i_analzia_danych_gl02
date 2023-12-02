0cdb89af3436d54a9db190774f6e88e996f618d6
Mikolaj Bialas <mikolajbialas01@wp.pl>
2023-12-02 13:50:40 +0100

    quick fix

--- zad_1.py
+++ zad_1.py
@@ -1,24 +1,22 @@
 from typing import List
 
+
 class Student:
     def __init__(self, name: str, marks: List):
         self.name = name
         self.marks = marks
-
 
     def is_passed(self) -> bool:
         avg = 0
         for mark in self.marks:
             avg += mark
 
-        avg = avg/len(self.marks)
+        avg = avg / len(self.marks)
 
         if avg > 50:
             return True
         else:
             return False
-
-
 
 
 student_01 = Student('Maciek', [80, 100, 70])
